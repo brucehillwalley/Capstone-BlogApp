ad9a4027706d930a9806a25c3045a1f954925d1d
BruceHill <brucehillwalley@gmail.com>
2024-04-25 03:23:56 +0300

    create pages and route

--- FE/blogapp/src/App.jsx
+++ FE/blogapp/src/App.jsx
@@ -1,6 +1,21 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Blogs from "./pages/Blogs";
 export default function App() {
   return (
-    <h1 className="text-3xl text-red-500 font-bold underline">App</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/blogs" element={<Blogs />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
