ddb10c8d050012d059f126af2b96bd9e06405c2f
BruceHill <brucehillwalley@gmail.com>
2024-04-25 05:26:15 +0300

    create a header section

--- FE/blogapp/src/App.jsx
+++ FE/blogapp/src/App.jsx
@@ -5,9 +5,11 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Blogs from "./pages/Blogs";
+import Header from "./components/Header";
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
