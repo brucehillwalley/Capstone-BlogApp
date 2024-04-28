e074d023079dfdb08fbae7a697ef9fa9258519f7
BruceHill <brucehillwalley@gmail.com>
2024-04-29 01:35:44 +0300

    up

--- BE/blogApi/src/helpers/sync.js
+++ BE/blogApi/src/helpers/sync.js
@@ -12,68 +12,64 @@ const User = require("../models/user");
 /*-------------------------------------------------------*/
 
 class MyLodash {
-    constructor() {}
-    static sample(arr) {
-      if (!arr || !arr.length) {
-        return undefined;
-      }
-      const randomIndex = Math.floor(Math.random() * arr.length);
-      return arr[randomIndex];
+  constructor() {}
+  static sample(arr) {
+    if (!arr || !arr.length) {
+      return undefined;
     }
-  
-    static random(min, max) {
-      if (min === undefined || max === undefined) {
-        throw new Error("Both min and max values are required.");
-      }
-      if (min > max) {
-        throw new Error("min must be less than or equal to max.");
-      }
-      return Math.floor(Math.random() * (max - min + 1) + min);
-    }
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
   }
-  
-  // console.log(MyLodash.random(0,5));
-  /*-------------------------------------------------------*/
-  //? USERS...........................................................
+
+  static random(min, max) {
+    if (min === undefined || max === undefined) {
+      throw new Error("Both min and max values are required.");
+    }
+    if (min > max) {
+      throw new Error("min must be less than or equal to max.");
+    }
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+}
+
+// console.log(MyLodash.random(0,5));
+/*-------------------------------------------------------*/
+//? USERS...........................................................
 const transferUsersCollection = async () => {
-    const { data } = await axios.get(`https://dummyjson.com/users`);
-    const dummyUsers = data.users;
+  const { data } = await axios.get(`https://dummyjson.com/users`);
+  const dummyUsers = data.users;
+  await User.create({
+    username: "admin",
+    password: "Bruce123*",
+    email: "admin@site.com",
+    isAdmin: true,
+  });
+
+  dummyUsers.forEach(async (user) => {
     await User.create({
-      username: "admin",
-      password: "Bruce123*",
-      email: "admin@site.com",
-      isAdmin: true,
-    })
-  
-    dummyUsers.forEach(async (user) => {
-      await User.create({
-        ...user,
-        id:null,
-        password: user.password + "*123",
-      });
+      ...user,
+      id: null,
+      password: user.password + "*123",
     });
-    console.log(
-      `${dummyUsers.length} users successfully transferred to the database.`
-    );
-  };
+  });
+  console.log(
+    `${dummyUsers.length} users successfully transferred to the database.`
+  );
+};
 
 //? CATEGORIES.......................................................
 const Category = require("../models/category");
 
-
-const categoriesData = require('./categoryData');
-
+const categoriesData = require("./categoryData");
 
 async function insertCategories() {
   for (const categoryData of categoriesData) {
-
     let subcategories = []; // Alt kategori nesnelerini tutmak için dizi
 
     let category = new Category({
       name: categoryData.name,
-      slug: categoryData.name.toLowerCase().replace(/\s+/g, '-'), // Slug'ı adından oluştur
+      slug: categoryData.name.toLowerCase().replace(/\s+/g, "-"), // Slug'ı adından oluştur
       // ... diğer kategori özellikleri
-   
     });
     await category.save();
     console.log(`- Category ${category.name} created.`);
@@ -82,19 +78,19 @@ async function insertCategories() {
       for (const subcategoryData of categoryData.subcategories) {
         const subcategory = new Category({
           name: subcategoryData.name,
-        
+
           // ... diğer alt kategori özellikleri
         });
 
         subcategory.parentCategoryIds.push(category._id);
         await subcategory.save();
         console.log(`-    Subcategory ${subcategory.name} created.`);
-        if(subcategoryData.subcategories){
+        if (subcategoryData.subcategories) {
           let subsubcategories = [];
           for (const subsubcategoryData of subcategoryData.subcategories) {
             const subsubcategory = new Category({
               name: subsubcategoryData.name,
-             
+
               // ... diğer alt kategori özellikleri
             });
 
@@ -102,48 +98,40 @@ async function insertCategories() {
             await subsubcategory.save();
             subsubcategories.push(subsubcategory._id);
 
-            console.log(`-        Subsubcategory ${subsubcategory.name} created.`);
+            console.log(
+              `-        Subsubcategory ${subsubcategory.name} created.`
+            );
           }
           subcategory.subCategoryIds = subsubcategories;
           await subcategory.save();
         }
 
-        
-        
-
-
         subcategories.push(subcategory._id); // Alt kategori _id'sini subcategories dizisine ekle
-       
       }
     }
 
-    category.subCategoryIds= subcategories,
-    await category.save();
-    
+    (category.subCategoryIds = subcategories), await category.save();
   }
 }
 
-
 //? DROP DATABASE....................................................
 async function cleanCollections() {
-    try {
-      const { mongoose } = require("../configs/dbConnection");
-      await mongoose.connection.dropDatabase();
-      console.log("- Database and all data DELETED!");
-    } catch (error) {
-      console.log("- ERROR: Database and all data NOT DELETED", error);
-    }
+  try {
+    const { mongoose } = require("../configs/dbConnection");
+    await mongoose.connection.dropDatabase();
+    console.log("- Database and all data DELETED!");
+  } catch (error) {
+    console.log("- ERROR: Database and all data NOT DELETED", error);
   }
-  /*-------------------------------------------------------*/
+}
+/*-------------------------------------------------------*/
 
-  module.exports = async () => {
- 
-    await cleanCollections();
-    try {
-      await transferUsersCollection();
-      await insertCategories();
-    
-    } catch (error) {
-      console.log("- ERROR: Transfer Failed ", error);
-    }
-  };
+module.exports = async () => {
+  await cleanCollections();
+  try {
+    await transferUsersCollection();
+    await insertCategories();
+  } catch (error) {
+    console.log("- ERROR: Transfer Failed ", error);
+  }
+};
