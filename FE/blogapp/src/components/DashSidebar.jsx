3dc55866ace5864e710ce353e586eecae5675a1c
BruceHill <brucehillwalley@gmail.com>
2024-05-03 09:27:16 +0300

    sidebar dashboard

--- FE/blogapp/src/components/DashSidebar.jsx
+++ FE/blogapp/src/components/DashSidebar.jsx
@@ -1,18 +1,31 @@
 import { Sidebar, SidebarItem, SidebarItemGroup, SidebarItems } from "flowbite-react";
 import { HiUser, HiArrowSmRight } from "react-icons/hi";
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
 
 export default function DashSidebar() {
+    const location = useLocation();
+    const [tab, setTab] = useState('');
+    useEffect(() => {
+      const urlParams = new URLSearchParams(location.search);
+      const tabFromUrl = urlParams.get('tab');
+      if (tabFromUrl) {
+        setTab(tabFromUrl);
+      }
+    }, [location.search]);
   return (
-    <Sidebar>
+    <Sidebar className='w-full md:56'>
     <SidebarItems>
         <SidebarItemGroup>
-          <SidebarItem active icon={HiUser} label={"User"} labelColor="dark">
+        <Link to="/dashboard?tab=profile">
+          <SidebarItem active={tab === 'profile'} icon={HiUser} label={"User"} labelColor="dark">
                 Profile
           </SidebarItem>
-          <SidebarItem active icon={HiArrowSmRight} className='cursor-pointer'>
+     </Link>
+          <SidebarItem active={tab === 'logout'} icon={HiArrowSmRight} className='cursor-pointer'>
                 Logout
           </SidebarItem>
-     
         </SidebarItemGroup>
 
     </SidebarItems>
