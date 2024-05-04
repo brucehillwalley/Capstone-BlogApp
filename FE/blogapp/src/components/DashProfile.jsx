8bf7624ecac0634ce42422f38f6935520ff2783c
BruceHill <brucehillwalley@gmail.com>
2024-05-05 00:29:58 +0300

    complete user profile image upload

--- FE/blogapp/src/components/DashProfile.jsx
+++ FE/blogapp/src/components/DashProfile.jsx
@@ -1,20 +1,144 @@
-import { Button, TextInput } from "flowbite-react";
+import { Alert, Button, Modal, ModalBody, TextInput } from "flowbite-react";
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import {
+  getDownloadURL,
+//   getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+//   ref,
+//   uploadBytesResumable,
+} from 'firebase/storage';
+import { app } from "../firebase";
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+// import {
+//   updateStart,
+//   updateSuccess,
+//   updateFailure,
+//   deleteUserStart,
+//   deleteUserSuccess,
+//   deleteUserFailure,
+//   signoutSuccess,
+// } from '../redux/user/userSlice';
+import { useDispatch } from "react-redux";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
+import { Link } from "react-router-dom";
 
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const filePickerRef = useRef(null);
+  const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
 
+  const uploadImage = async () => {
+    // service firebase.storage {
+    //   match /b/{bucket}/o {
+    //     match /{allPaths=**} {
+    //       allow read; 
+    //       allow write: if
+    //       request.resource.size < 2 * 1024 * 1024 &&
+    //       request.resource.contentType.matches('image/.*')
+    //     }
+    //   }
+    // }
+    setImageFileUploadError(null);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setImageFileUploadProgress(progress.toFixed(0));
+          // console.log("Upload is " + imageFileUploadProgress + "% done\n", imageFileUploadError);
+
+        // switch (snapshot.state) {
+        //   case "paused":
+        //     console.log("Upload is paused");
+        //     break;
+        //   case "running":
+        //     console.log("Upload is running");
+        //     break;
+        //   default:
+        //     break;
+        // }
+      },
+      (error) => {
+        setImageFileUploadError( 'Could not upload image (File must be less than 2MB)');
+        setImageFileUploadProgress(null);
+        setImageFile(null);
+        setImageFileUrl(null);
+
+      },
+      () => {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageFileUrl(downloadURL);
+        }); 
+      }
+    )
+  }
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
       <form className="flex flex-col gap-4">
-        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+        <input type="file" accept="image/*" onChange={handleImageChange} ref={filePickerRef} hidden/>
+        {/* resme tıklandığıda input a tıklanmıs olacak */}
+        <div className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full" onClick={() => filePickerRef.current.click()}>
+        {imageFileUploadProgress && (
+          <CircularProgressbar value={imageFileUploadProgress || 0} text={`${imageFileUploadProgress}%`} 
+            strokeWidth={5}
+            styles={{
+              root: { 
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+                top: 0,
+                left: 0,
+
+                },
+              path: {
+                // transparency progress e bağlı olarak azalacak tamamlandığında tam rengi vericez
+                stroke: `rgba(62, 152, 199, ${imageFileUploadProgress / 100})`,
+
+              }
+            }}
+          />
+        )
+        }
           <img
-            src={currentUser?.profilePicture}
+            src={imageFileUrl || currentUser?.profilePicture}
             alt="user"
-            className="rounded-full w-full object-cover h-full border-8 border-[lightgray]"
+            className={`rounded-full w-full object-cover h-full border-8 border-[lightgray] ${imageFileUploadProgress && imageFileUploadProgress < 100 && "opacity-60"}`}
           />
         </div>
+          {imageFileUploadError && (
+            
+        <Alert color='failure'>
+        {imageFileUploadError}
+        </Alert>
+          )}
         <TextInput
           type="text"
           id="username"
@@ -33,20 +157,16 @@ export default function DashProfile() {
           type="password"
           id="password"
           placeholder="password"
-          
+
           //   onChange={handleChange}
         />
         <Button gradientDuoTone="purpleToBlue" type="submit" outline>
           Update
         </Button>
-        <div className='text-red-500 flex justify-between mt-5'>
-        <span  className='cursor-pointer'>
-          Delete Account
-        </span>
-        <span  className='cursor-pointer'>
-          Sign Out
-        </span>
-      </div>
+        <div className="text-red-500 flex justify-between mt-5">
+          <span className="cursor-pointer">Delete Account</span>
+          <span className="cursor-pointer">Sign Out</span>
+        </div>
       </form>
     </div>
   );
--- FE/blogapp/src/components/DashSidebar.jsx
+++ FE/blogapp/src/components/DashSidebar.jsx
@@ -1,37 +1,47 @@
-import { Sidebar, SidebarItem, SidebarItemGroup, SidebarItems } from "flowbite-react";
+import {
+  Sidebar,
+  SidebarItem,
+  SidebarItemGroup,
+  SidebarItems,
+} from "flowbite-react";
 import { HiUser, HiArrowSmRight } from "react-icons/hi";
-import { useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 export default function DashSidebar() {
-    const location = useLocation();
-    const [tab, setTab] = useState('');
-    useEffect(() => {
-      const urlParams = new URLSearchParams(location.search);
-      const tabFromUrl = urlParams.get('tab');
-      if (tabFromUrl) {
-        setTab(tabFromUrl);
-      }
-    }, [location.search]);
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
   return (
-    <Sidebar className='w-full md:56'>
-    <SidebarItems>
+    <Sidebar className="w-full md:56">
+      <SidebarItems>
         <SidebarItemGroup>
-        <Link to="/dashboard?tab=profile">
-          <SidebarItem active={tab === 'profile'} icon={HiUser} label={"User"} labelColor="dark">
-                Profile
-          </SidebarItem>
-     </Link>
-          <SidebarItem active={tab === 'logout'} icon={HiArrowSmRight} className='cursor-pointer'>
-                Logout
+          <Link to="/dashboard?tab=profile">
+            <SidebarItem
+              active={tab === "profile"}
+              icon={HiUser}
+              label={"User"}
+              labelColor="dark"
+              as="div"
+            >
+              Profile
+            </SidebarItem>
+          </Link>
+          <SidebarItem
+            active={tab === "logout"}
+            icon={HiArrowSmRight}
+            className="cursor-pointer"
+          >
+            Logout
           </SidebarItem>
         </SidebarItemGroup>
-
-    </SidebarItems>
-    
-    
+      </SidebarItems>
     </Sidebar>
-  )
+  );
 }
-
