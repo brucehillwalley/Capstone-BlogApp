a1cbea390ec79966efcbf1e0e5d8775ced40b1c1
BruceHill <brucehillwalley@gmail.com>
2024-05-07 00:59:27 +0300

    show my activities

--- FE/blogapp/src/components/DashMyActivity.jsx
+++ FE/blogapp/src/components/DashMyActivity.jsx
@@ -1,9 +1,17 @@
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import useAxios from "../service/useAxios";
+import { Modal, Table, Button } from 'flowbite-react';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
+// import { set } from 'mongoose';
+import useAxios from '../service/useAxios';
 
 export default function DashMyActivity() {
-  const { currentUser } = useSelector((state) => state.user);
+    const { currentUser } = useSelector((state) => state.user);
+    const [userPosts, setUserPosts] = useState([]);
+    const [showMore, setShowMore] = useState(true);
+    const [showModal, setShowModal] = useState(false);
+    const [postIdToDelete, setPostIdToDelete] = useState('');
   const { axiosWithToken } = useAxios();
   const [activities, setActivities] = useState([]);
   useEffect(() => {
@@ -18,9 +26,10 @@ export default function DashMyActivity() {
             currentUser._id
           }`
         );
-        console.log(res.data);
+        console.log(res.data.data);
         if (!res.data.error) {
-          setActivities(res.data);
+          setActivities(res.data.data);
+          
         }
       } catch (error) {
         console.log(error);
@@ -28,6 +37,83 @@ export default function DashMyActivity() {
     };
     getActivities();
   }, []);
+ 
+  return <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
 
-  return <div>DashMyActivity</div>;
+    {activities.length > 0 ? (
+        <>
+          <Table hoverable className='shadow-md'>
+            <Table.Head>
+              <Table.HeadCell>
+                Date updated
+              </Table.HeadCell>
+              <Table.HeadCell>
+               Image
+              </Table.HeadCell>
+              <Table.HeadCell>
+               Title
+              </Table.HeadCell>
+              <Table.HeadCell>
+                Category
+              </Table.HeadCell>
+              <Table.HeadCell>
+                Delete
+              </Table.HeadCell>
+              <Table.HeadCell>
+                <span className="">Edit</span>
+              </Table.HeadCell>
+            </Table.Head>
+            {activities.map((activity) => (
+                <Table.Body key={activity._id} className='divide-y'>
+
+                  <Table.Row  className='bg-white dark:border-gray-700 dark:bg-gray-800' >
+
+                    <Table.Cell>
+                    {new Date(activity.updatedAt).toLocaleDateString()}
+                    </Table.Cell>
+                    <Table.Cell>
+                    <Link to={`/activity/${activity.slug}`}>
+                      <img
+                        src={activity.image}
+                        alt={activity.title}
+                        className='w-20 h-10 object-cover bg-gray-500'
+                      />
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link className='font-medium text-gray-900 dark:text-white' to={`/activity/${activity.slug}`}>
+                      {activity.title}
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>
+                  {/* TODO: backend e populate yap */}
+                    {activity.categoryName}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <span className='font-medium text-red-500 hover:underline cursor-pointer'>
+                        Delete
+                    </span>
+                  </Table.Cell>
+                  <Table.Cell>
+                  <Link className='text-teal-500' to={`/update-activity/${activity._id}`}>
+                    <span className='hover:underline'>
+                        Edit
+                    </span>
+                  </Link>
+                  </Table.Cell>
+                   
+
+
+                  </Table.Row>
+
+                </Table.Body>
+            ))}
+            
+          </Table>
+        </>
+    ) : (
+        <p>You have no posts yet!</p>
+    )}
+
+  </div>;
 }
