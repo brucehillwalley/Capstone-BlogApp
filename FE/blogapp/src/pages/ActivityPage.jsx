053a153e4c1f8e94fbcc5b5829f38a4e1c5a07cc
BruceHill <brucehillwalley@gmail.com>
2024-05-09 01:02:57 +0300

    CallToAction

--- FE/blogapp/src/pages/ActivityPage.jsx
+++ FE/blogapp/src/pages/ActivityPage.jsx
@@ -1,7 +1,7 @@
 import { Button, Spinner } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-// import CallToAction from '../components/CallToAction';
+import CallToAction from '../components/CallToAction';
 // import CommentSection from '../components/CommentSection';
 // import activityCard from '../components/activityCard';
 import useAxios from "../service/useAxios";
@@ -90,10 +90,10 @@ export default function ActivityPage() {
         className="p-3 max-w-2xl mx-auto w-full activity-content"
         dangerouslySetInnerHTML={{ __html: activity && activity.content }}
       ></div>
-      {/* <div className='max-w-4xl mx-auto w-full'>
+      <div className='max-w-4xl mx-auto w-full'>
         <CallToAction />
       </div>
-      <CommentSection activityId={activity._id} /> */}
+      {/* <CommentSection activityId={activity._id} /> */}
 
       <div className="flex flex-col justify-center items-center mb-5">
         <h1 className="text-xl mt-5">Recent articles</h1>
