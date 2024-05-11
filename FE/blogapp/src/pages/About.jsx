6e97caeaf98233dee9a15756315030aeb533e442
BruceHill <brucehillwalley@gmail.com>
2024-05-12 01:15:07 +0300

    About

--- FE/blogapp/src/pages/About.jsx
+++ FE/blogapp/src/pages/About.jsx
@@ -1,5 +1,26 @@
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About Activity Blog
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Welcome to Activity Blog! This blog was created to share our passion for activities and inspire you to explore a world of exciting experiences. We believe that activities are not just pastimes; they are powerful tools for personal growth, self-discovery, and community connection.
+            </p>
+
+            <p>
+              On this blog, you'll find a variety of articles and tutorials on topics related to activities your city and beyond. We aim to provide curated guides, engaging content, and inspiring stories to help you discover and enjoy the best activities your city has to offer.
+            </p>
+
+            <p>
+              We encourage you to join our thriving community! Share your experiences, connect with fellow activity enthusiasts, and discover new possibilities together. Let's embark on a journey of exploration and create lasting memories through engaging activities.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
